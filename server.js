a4f930907884bf17485a58944d91e53e302fffd2
jarozweit <jarozweit@gmail.com>
2026-02-18 13:14:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,14 +9,11 @@ app.use(express.static("public")); // Statische Dateien ausliefern
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
-    secure: false,
+    secure: true,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
     },
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000
 });
 
 // API‑Route für Bestellungen
